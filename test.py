278907c53692971164642b6f5fe951d49ee04c7a
vladjavadev <vharcuk02@gmail.com>
2025-12-02 13:27:37 +0200

    test commit

---
+++ test.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+import time
+
+# Номер піна в BCM-номерації (наприклад GPIO18 підтримує апаратний PWM)
+PWM_PIN = 18
+
+# Налаштування GPIO
+GPIO.setmode(GPIO.BCM)       # Використовуємо BCM-нумерацію
+GPIO.setup(PWM_PIN, GPIO.OUT)
+
+# Створюємо PWM-об'єкт з частотою 1000 Гц
+pwm = GPIO.PWM(PWM_PIN, 1000)
+pwm.start(0)  # Починаємо з duty cycle = 0%
+
+try:
+    while True:
+        # Плавне збільшення яскравості (наприклад для LED)
+        for duty in range(0, 101, 5):
+            pwm.ChangeDutyCycle(duty)
+            time.sleep(0.5)
+
+        # Плавне зменшення
+        for duty in range(100, -1, -5):
+            pwm.ChangeDutyCycle(duty)
+            time.sleep(0.1)
+
+except KeyboardInterrupt:
+    pass
+
+# Завершення роботи
+pwm.stop()
+GPIO.cleanup()
